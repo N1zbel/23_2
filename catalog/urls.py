4363bbc03ee2a6566698f4bf5684aa6300473ad2
Andrey Ryazanov <ark2635@gmail.com>
2023-09-13 16:52:04 +0300

    fbc -> cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
-import products as products
 from django.urls import path
 
-from catalog.views import home, contacts, detail_product
+from catalog.views import home, contacts, page_not_found, CatalogListView, ProductDetailView
+
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
-    path('product/<pk>', detail_product)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<pk>', ProductDetailView.as_view(), name='product_number'),
+    path('catalog/', CatalogListView.as_view(), name='catalog')
+
 ]
