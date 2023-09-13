68576a3d0476173b910e6cf90925e97df85b7978
Andrey Ryazanov <ark2635@gmail.com>
2023-09-13 17:06:33 +0300

    fbv-cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from catalog.views import home, contacts, page_not_found, CatalogListView, ProductDetailView
+from catalog.views import home, contacts, CatalogListView, ProductDetailView
 
 app_name = 'catalog'
 
