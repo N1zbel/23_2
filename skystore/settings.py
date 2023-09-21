530fdf694b612a230a2babc9a6046e17b2d61627
Andrey Ryazanov <ark2635@gmail.com>
2023-09-21 18:00:22 +0300

    upd settings.py

--- skystore/settings.py
+++ skystore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p=!_#(v811qqi&hcgv5b)$xhsmegx&^*abiq0soo$2phvq1ask
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
